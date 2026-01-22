6b9aec7813dbbc6b03527949b34ff82b28be4550
Daniel Tsvetkov <dcvetkov7@gmail.com>
2026-01-22 08:57:01 +0000

    dbt_project.yml file table settings fix

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -7,15 +7,18 @@ WITH daily_weather AS (
         humidity,
         clouds
       FROM {{ source('DEMO', 'weather') }}
-      LIMIT 10
 ),
 daily_weather_agg AS (
     select 
         daily_weather,
         weather,
-        count(weather)
+        round(avg(temp), 2) as avg_temp,
+        round(avg(pressure), 2) as avg_pressure,
+        round(avg(humidity), 2) as avg_humidity,
+        round(avg(clouds), 2) as avg_clouds
      from daily_weather
     group by daily_weather, weather
+    qualify ROW_NUMBER () OVER (PARTITION BY daily_weather ORDER BY count(weather) DESC) = 1
 )
 SELECT *
 FROM daily_weather_agg
