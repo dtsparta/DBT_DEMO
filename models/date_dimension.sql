cc2633a0288c306a281d4cf6b174a5871c2b25e9
Daniel Tsvetkov <dcvetkov7@gmail.com>
2026-01-22 08:36:05 +0000

    daily station dimension added

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,14 +1,13 @@
-WITH CTE AS (
-select
-TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
-DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
-HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
-{{day_type('STARTED_AT')}} AS DAY_TYPE,
-{{get_season('STARTED_AT')}} AS STATION_OF_YEAR
-from
-{{ source('DEMO', 'bike') }}
-where STARTED_AT != 'started_at' and STARTED_AT != '"started_at"'
-)
-select 
-*
-from CTE
+with
+    cte as (
+        select
+            to_timestamp(started_at) as started_at,
+            date(to_timestamp(started_at)) as date_started_at,
+            hour(to_timestamp(started_at)) as hour_started_at,
+            {{ day_type("STARTED_AT") }} as day_type,
+            {{ get_season("STARTED_AT") }} as station_of_year
+        from {{ source("DEMO", "bike") }}
+        where started_at != 'started_at' and started_at != '"started_at"'
+    )
+select *
+from cte
